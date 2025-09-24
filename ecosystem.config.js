bb80145ae63334ee99b5fcdc82c2ccf4646367ed
Karula-k <d.fikriirsyad@gmail.com>
2025-09-24 09:48:12 +0700

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
+require("dotenv").config();
 
 const port = process.env.PM2_PORT;
 
 module.exports = {
   apps: [
     {
-      name: 'nextjs-vuexy-kit',
+      name: "kalimasada",
       script: `npm start -- -p ${port}`,
-      error_file: 'log/error.log',
-      out_file: 'log/out.log',
-      args: '',
-      node_args: '--tls-min-v1.0',
+      error_file: "log/error.log",
+      out_file: "log/out.log",
+      args: "",
+      node_args: "--tls-min-v1.0",
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '300M'
+      max_memory_restart: "300M"
     }
   ],
   deploy: {}
